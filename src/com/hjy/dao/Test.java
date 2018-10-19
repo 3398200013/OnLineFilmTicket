5c7b29bcd3e93944defac0214b917601099e8e51
huangjiabao <443809896@qq.com>
2018-10-19 14:07:11 +0800

    Update src/com/hjy/dao/Test.java

--- src/com/hjy/dao/Test.java
+++ src/com/hjy/dao/Test.java
@@ -2,10 +2,9 @@ package com.hjy.dao;
 
 public class Test {
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		System.out.println("aaaa");
-=======
+
 		System.out.println("hahaha");
->>>>>>> 9538245338dbc5c00f6637d2fe12fd1843457793
 	}
 }
